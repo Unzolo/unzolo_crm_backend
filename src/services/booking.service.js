5d985476d4421360d93a034cbe3a1340d2daf4b3
muhammedrafeeq <muhammed.rafeeq@themarblejar.io>
2026-02-04 10:08:53 +0530

    fixed minor issue

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -243,7 +243,7 @@ const getBookingById = async (id, partnerId) => {
       },
       {
         model: Partner,
-        attributes: ['companyName', 'phoneNumber']
+        attributes: ['name', 'phone']
       }
     ],
   });
